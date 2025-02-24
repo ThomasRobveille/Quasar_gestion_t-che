01f4750c0745ebc7bde1db47f8a33d9174f129e5
thomasrobveille <thomasrobveille@gmail.com>
2025-02-24 10:26:24 +0100

    premier commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <h1>bonjour je suis la </h1>
 </template>
 
 <script setup>
